be66794015c65347a31332e13d67e7f7bd9d9f24
rumblekat <reki318@naver.com>
2026-07-11 15:04:52 +0900

    cleanup: remove unused react components, redux actions, reducers, and style files

--- src/actions/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const CEOINTRO = "CEOINTRO";
-export const ORGANIZATION = "ORGANIZATION";
-export const TIMELINE = "TIMELINE";
-export const TRANSLATE = "TRANSLATE";
--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import * as types from "./ActionTypes";
-
-export function TIMELINE (){
-    return {
-        type: types.TIMELINE
-    }
-} 
-
